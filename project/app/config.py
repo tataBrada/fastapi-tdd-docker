4e5957499b026d998206771b01c11f738c9ea666
Bosko Savic <bosko.savic@serengetitech.com>
2023-07-07 12:53:22 +0200

    setup postgres, tortoise

--- project/app/config.py
+++ project/app/config.py
@@ -1,6 +1,6 @@
 import logging
 from functools import lru_cache
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 
 log = logging.getLogger("uvicorn")
@@ -10,6 +10,8 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = bool(0)
+    database_url: AnyUrl = None
+
 
 @lru_cache()
 def get_settings() -> BaseSettings:
