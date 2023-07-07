ad4c6bf7ba9612dc4a3d9c0fba61b81ccb13946d
Bosko Savic <bosko.savic@serengetitech.com>
2023-07-07 14:32:34 +0200

    add get, post routes,  add tests

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ import logging
 import os
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from .api import ping
+from .api import ping, summaries
 from app.db import init_db
 
 logger = logging.getLogger("uvicorn")
@@ -11,6 +11,7 @@ logger = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,4 +1,5 @@
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class TextSummary(models.Model):
@@ -9,3 +10,5 @@ class TextSummary(models.Model):
     def __str__(self):
         return self.url
 
+
+SummarySchema = pydantic_model_creator(TextSummary)
