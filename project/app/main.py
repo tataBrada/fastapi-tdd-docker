754dc01b8630373e51732398f9d12d6a6347aa93
Bosko Savic <bosko.savic@serengetitech.com>
2023-07-07 13:41:16 +0200

    refactor app, add APIRouter, pydantic model

--- project/app/main.py
+++ project/app/main.py
@@ -1,24 +1,30 @@
+import logging
 import os
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
+from .api import ping
+from app.db import init_db
 
-from app.config import get_settings, Settings
-
-app = FastAPI()
-
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=False,
-    add_exception_handlers=True,
-)
+logger = logging.getLogger("uvicorn")
 
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(ping.router)
+
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.info("Shutting down...")
+
